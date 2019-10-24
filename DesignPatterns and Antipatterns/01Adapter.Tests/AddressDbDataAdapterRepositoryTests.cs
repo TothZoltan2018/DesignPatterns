0269bb24a01b3f39a1834ffb439253ca11f0bfab
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2019-10-24 21:55:19 +0200

    #1: Adapter minta, IDbDataAdapter

---
+++ DesignPatterns and Antipatterns/01Adapter.Tests/AddressDbDataAdapterRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _01Adapter.Tests
+{
+    /// <summary>
+    /// Repository ----()-----> Adatok (adatbazis)
+    /// A fenti eros csatolas helyett indirekcioval:
+    /// Repository ----> IDbDataAdapter  -----Adatok (adatbazis)
+    /// 
+    /// </summary>
+
+
+    [TestClass]
+    public class AddressDbDataAdapterRepositoryTests
+    {
+        [TestMethod]
+        public void AddressDbDataAdapterRepositoryShouldThrowExceptionIfArgumentNull()
+        {
+            //Arrange
+            AddressDbDataAdapterRepository sut;//sut: System Under Test
+
+            //Act
+            Action todo = () => sut = new AddressDbDataAdapterRepository(null);
+
+            //Assert
+            todo.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
---
+++ DesignPatterns and Antipatterns/01Adapter/AddressDbDataAdapterRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using System;
+using System.Collections.Generic;
+
+namespace _01Adapter
+{
+    public class AddressDbDataAdapterRepository : IAddressRepository
+    {
+        private IDbDataAdapter adapter; //IDbDataAdapter: A .NET resze
+
+        public AddressDbDataAdapterRepository(IDbDataAdapter adapter)
+        {
+            this.adapter = adapter ?? throw new ArgumentNullException(nameof(adapter));
+        }
+
+        public IList<Address> GetAddresses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
