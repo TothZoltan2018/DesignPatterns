b5c27ed91edfb4518de661a6891b73f90387d844
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2019-11-11 09:44:22 +0100

    #4:Bejaro tovabbi gondolatok    - parhuzamos bejaras    - modositas bejaras alatt

--- DesignPatterns and Antipatterns/04Iterator2/Program.cs
+++ DesignPatterns and Antipatterns/04Iterator2/Program.cs
@@ -15,8 +15,22 @@ namespace _04Iterator2
             bejarhatoOsztaly.Add(new SajatOsztaly("Negyedik bejegyzes"));
 
             foreach (var item in bejarhatoOsztaly)
-            {
+            {                
                 Console.WriteLine(item.Uzenet);
+                //A bejaro nem teljes gyujtemenyeleres, ezert iyet nem tehetunk:                
+                //bejarhatoOsztaly.Remove(item);
+                //De ha megis:
+                bejarhatoOsztaly.Remove(item);
+                //tudnom kell arrol, hogy ha az adatokat modositjak,
+                //nem ertesulok rola, es csak akkor tudok ellene tenni, 
+                //ha erre a forgatokonyvre felkeszulok:
+
+                //1. Nem teszunk listat a kirakatba (nem tesszuk elerhetove a lista adatainkat kivulrol)
+                //2. Igy ertesulunk a modositasrol, es kezelni tudjuk
+                //3. Helyzettol fugg, hogy engedjuk -e a modositast, es ertesitjuk a bejaro(ka)t, vagy mar a modositast sem engedjuk
+                //4. Parhuzamos bejarasokra a sajat bejaroval lehet a legegyszerubben felkeszulni
+                //5. Ha a bejaro es a bejarhato egy osztalyba kerul, akkor a dispose() implaemntacioja tul bonyolult lehet 
+
             }
             //Miutan a foreach veget er, megivja a Dispose() fgvt.
 
@@ -44,6 +58,20 @@ namespace _04Iterator2
             //    }                
             //}
 
+
+            ////Nagyon fontos annak kezelese, hogy a bejarhato peldeny elemei modosultak -e?
+            ////Mert ha igen, akkor valamit tenni kell. A List<T> Enumerator-a peldaul exception-t dob
+            //var lista = new List<string>(new string[] { "egy", "ketto", "harom" });
+            ////var lista = new List<string>() { "egy", "ketto", "harom" }; //ugyanaz, mint elobb
+
+            //foreach (var item in lista)
+            //{
+            //    Console.WriteLine($"elem: {item}");
+            //    lista.Remove(item); //-->
+            //--> System.InvalidOperationException: 'Collection was modified; enumeration operation may not execute.'
+
+            //}
+
             Console.ReadLine();
         }
     }
@@ -68,11 +96,18 @@ namespace _04Iterator2
 
     class BejarhatoOsztaly<T> : IEnumerable<T>
     {
+        //Antipattern pelda: listat nem osztunk meg!
         List<T> list = new List<T>();
 
-        internal void Add(T item)
+        public void Add(T item)
         {
             list.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            list.Remove(item);
+            //ertesitenem kell a bejaroimat, hogy helyzet van!!!
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -84,6 +119,7 @@ namespace _04Iterator2
         {//ez egyszeru duplikalas, csak meghivom a tipusos fuggvenyt
             return this.GetEnumerator();
         }
+
     }
 
     class BejaroOsztaly<T> : IEnumerator<T>
