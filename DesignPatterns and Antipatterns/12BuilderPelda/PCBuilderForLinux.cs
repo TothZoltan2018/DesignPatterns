0e136074e0985baea1fb6c6a55d40054f929c7a7
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-01-20 17:09:54 +0100

    #10: Masodik konfiguracio kialakitasa

---
+++ DesignPatterns and Antipatterns/12BuilderPelda/PCBuilderForLinux.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12BuilderPelda
+{
+    public class PCBuilderForLinux : AbstractPCBuilder
+    {
+        public override void InstallApplications()
+        {
+            computer.Applications = new List<string> { "Firefox", "Thunderbird", "VLC", "Visual Studio Code" };
+        }
+
+        public override void InstallOS()
+        {
+            computer.OS = OS.Ubuntu16_10;
+        }
+
+        public override void BuildHardware()
+        {
+            computer.Processor = Processor.amd64;
+            computer.HDD = 240;
+            computer.HasDVD = false;
+            computer.HasSoundCard = false;
+            computer.HasUSB = true;
+        }
+    }
+}
