59a857b7831c3560c87af13ab360511e93935c32
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-01-20 15:08:54 +0100

    #10: A beazonositott lepeseket vazlatkent bezartuk egy absztrakt  osztalyba, es a prototipust atneveztuk.

---
+++ DesignPatterns and Antipatterns/12BuilderPelda/AbstractPCBuilder.cs
@@ -0,0 +1,33 @@
+﻿namespace _12BuilderPelda
+{
+    public abstract class AbstractPCBuilder
+    {
+        protected Computer computer;
+
+        public void CreatePC()
+        {
+            computer = new Computer();
+        }
+
+        public void BuildPC()
+        {
+            BuildHardware();
+            InstallOS();
+            InstallApplications();
+            //nem kell visszaterni, mert referencia valtzot vettunk at
+            //return computer;
+        }
+
+        public abstract void InstallApplications();
+
+        public abstract void InstallOS();
+
+        public abstract void BuildHardware();
+
+        public Computer GetPC()
+        {
+            return computer;
+        }
+
+    }
+}
---
+++ DesignPatterns and Antipatterns/12BuilderPelda/PCBuilderForWindows.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12BuilderPelda
+{
+    public class PCBuilderForWindows : AbstractPCBuilder
+    {
+        public override void InstallApplications()
+        {
+            computer.Applications = new List<string> { "MSSQL", "VisualStudio", "VLC" };
+        }
+
+        public override void InstallOS()
+        {
+            computer.OS = OS.Windows7;
+        }
+
+        public override void BuildHardware()
+        {
+            computer.Processor = Processor.x64;
+            computer.HDD = 120;
+            computer.HasDVD = true;
+            computer.HasSoundCard = true;
+            computer.HasUSB = true;
+        }
+    }
+}
